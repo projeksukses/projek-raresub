90a12225a002314a3fe2a903af23190b57205cfa
andromeda <andromedabubinga@gmail.com>
2025-08-15 09:36:10 +0700

    Fix: Page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">About Me</h1>
+      <p>Ini halaman About</p>
+    </div>
+  );
+}
---
+++ src/app/shop/page.tsx
@@ -0,0 +1,8 @@
+export default function ShopPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Shop</h1>
+      <p>Ini halaman Shop</p>
+    </div>
+  );
+}
