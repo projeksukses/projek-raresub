c1df25a2b17ae9cfb312609b92ee2dd416a90b1c
andromeda <andromedabubinga@gmail.com>
2025-08-15 09:46:06 +0700

    Fix: Page FIll

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ export default function ContactPage() {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold">Contact Me</h1>
-      <p>Ini halaman Contactt</p>
+      <p>Ini halaman Contact</p>
     </div>
   );
 }
