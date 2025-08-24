43571276de7dd3204cfbfa112968ca3030400c08
Ongky <ongkyselasetyawan@gmail.com>
2025-08-24 15:24:29 +0700

    feat: button

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/button/Button";
+
+export default function MainPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Hi!</h1>
+      <p>Ini halaman Home</p>
+      <div className="flex gap-4 mt-4 items-center">
+        <Button variant="primary">Primary Button</Button>
+        <Button variant="secondary">Secondary Button</Button>
+        <Button variant="ghost">Ghost Button</Button>
+      </div>
+    </div>
+  );
+}
